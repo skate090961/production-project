73b556451de5470ed4389ac0d46eb1839e69faf9
skate090961 <m4xwork@yandex.ru>
2025-08-29 17:38:14 +0400

    fix: fix api config 2

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,15 +2,13 @@ import axios from 'axios';
 
 import { USER_LS_KEY } from '@/shared/consts/local-storage';
 
-const authorization = localStorage.getItem(USER_LS_KEY) || '';
-
 export const $api = axios.create({
     baseURL: __API__,
 });
 
 $api.interceptors.request.use((config) => {
     if (config.headers) {
-        config.headers.Authorization = authorization;
+        config.headers.Authorization = localStorage.getItem(USER_LS_KEY) || '';
     }
 
     return config;
